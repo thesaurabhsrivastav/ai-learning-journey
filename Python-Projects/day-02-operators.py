9e031172e5610c98abe34ef21fc127a970c21a6f
thesaurabhsrivastav <hiiiamsaurabh@gmail.com>
2026-05-15 10:08:42 +0530

    Day-02 basic operators and conditions

---
+++ Python-Projects/day-02-operators.py
@@ -0,0 +1,19 @@
+a = 10
+b = 3
+
+print(a + b)   # Addition
+print(a - b)   # Subtraction
+print(a * b)   # Multiplication
+print(a / b)   # Division
+print(a % b)   # Modulus (remainder)
+print(a ** b)  # Power
+
+a = 10
+b = 20
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
