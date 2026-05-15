a17adc927bb6b0ea5c57634244fe2ee7be188657
thesaurabhsrivastav <hiiiamsaurabh@gmail.com>
2026-05-15 10:12:30 +0530

    Day-02 basic input login system

---
+++ Python-Projects/day-02-conditions.py
@@ -0,0 +1,19 @@
+marks = 75
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 70:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
+
+age = 25
+salary = 50000
+
+if age > 18 and salary > 30000:
+    print("Eligible")
+
+is_raining = False
+print(not is_raining)
