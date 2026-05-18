a8c2b7e336430ca41ae3dbb39ff694b4f0ea3598
thesaurabhsrivastav <hiiiamsaurabh@gmail.com>
2026-05-18 21:21:29 +0530

    day-08-basic String

---
+++ Python-Projects/day-08-Strings.py
@@ -0,0 +1,26 @@
+text = "Python"
+
+print(text[0])
+print(text[3])
+text = "I like Java"
+
+print(text.replace("Java", "Python"))
+sentence = "AI will change the world"
+
+words = sentence.split()
+
+print(words)
+words = ['AI', 'is', 'powerful']
+
+sentence = " ".join(words)
+
+print(sentence)
+
+name = "Rahul"
+
+print(f"Hello {name}")
+
+text = "AI"
+
+for char in text:
+    print(char)
