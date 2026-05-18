615d4357af61364160b1867be87d16590358ecba
thesaurabhsrivastav <hiiiamsaurabh@gmail.com>
2026-05-18 20:59:30 +0530

    day-05-basic Lists

---
+++ Python-Projects/day-06-lists.py
@@ -0,0 +1,14 @@
+numbers = [10, 20, 30, 40]
+
+print(numbers[0])
+print(numbers[2])
+print(numbers[-1]) #Last Index Number
+numbers = [10, 20, 30, 40, 50]
+
+print(numbers[1:4]) #Including 1st index and excluding 4th Index
+
+numbers = [1, 2, 3]
+
+numbers.append(4)
+
+print(numbers)
