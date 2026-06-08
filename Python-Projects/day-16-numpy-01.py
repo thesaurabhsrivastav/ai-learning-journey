8d9a147d65ca163bf8820e9fc17c684dcd3a30f1
thesaurabhsrivastav <hiiiamsaurabh@gmail.com>
2026-06-08 20:45:39 +0530

    day-16-numpy-01

---
+++ Python-Projects/day-16-numpy-01.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+print(np.__version__)
+
+marks = np.array([70, 80, 90, 95])
+
+print(marks)
+
+print(marks + 5)
+
+print(marks * 2)
+
+a = np.array([1, 2, 3])
+
+b = np.array([10, 20, 30])
+
+print(a + b)
+
+print("Original Marks:", marks)
+
+print("Grace Marks Added:", marks + 5)
+
+print("Double Marks:", marks * 2)
