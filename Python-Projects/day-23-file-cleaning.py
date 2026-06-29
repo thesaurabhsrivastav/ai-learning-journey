2ef3a717001f3d1a3706911e8eb44948ee70acd5
thesaurabhsrivastav <hiiiamsaurabh@gmail.com>
2026-06-29 21:37:36 +0530

    Exploratory Data Analysis (EDA)

--- Python-Projects/day-23-file-cleaning.py
+++ Python-Projects/day-23-file-cleaning.py
@@ -28,11 +28,19 @@ df["Marks"] = df["Marks"].fillna(0) #Fill Missing Values
 df["Marks"] = df["Marks"].fillna(df["Marks"].mean()) #Fill Using avg
 
 data = {
-    "Name": ["Rahul", "Rahul", "Aman"],
-    "Marks": [90, 90, 85]
+    "Name": ["Rahul", "Rahul", "Aman", "Saurabh"],
+    "Marks": [90, 90, 85, 70]
 }
 
+df = pd.DataFrame(data)
+
 print(df.duplicated())
+
+print(df["Marks"].unique())
+
+print(df["Marks"].nunique())
+
+print(df["Marks"].value_counts())
 
 df = df.drop_duplicates()
 
